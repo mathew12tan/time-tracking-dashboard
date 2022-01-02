93b97e05dcdb085de83b475434f7a463d97f8b78
mathew tan <mathew12tan@gmail.com>
2022-01-02 19:53:14 +0800

    fourth commit

--- app.js
+++ app.js
@@ -33,26 +33,23 @@ fetch("data.json")
       });
     };
 
-    intervalBtns.forEach((intervalBtn) => {
-      if (intervalBtn.getAttribute("aria-selected") === "true") {
-        let frequency = intervalBtn.innerHTML.toLowerCase();
-        frequencyValue(frequency);
-        frequencyText(frequency);
-      }
-    });
-
     titles.forEach((element, index) => {
       element.innerHTML = data[index].title;
     });
 
     intervalBtns.forEach((intervalBtn) => {
+      let frequency = intervalBtn.innerHTML.toLowerCase();
+
+      if (intervalBtn.getAttribute("aria-selected") === "true") {
+        frequencyValue(frequency);
+        frequencyText(frequency);
+      }
+
       intervalBtn.addEventListener("click", () => {
         intervalBtns.forEach((intervalBtn) => {
           intervalBtn.setAttribute("aria-selected", false);
         });
         intervalBtn.setAttribute("aria-selected", true);
-
-        let frequency = intervalBtn.innerHTML.toLowerCase();
         frequencyValue(frequency);
         frequencyText(frequency);
       });
